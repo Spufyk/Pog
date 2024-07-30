edc9ee2bdc759a766debfb61624683c17a90a325
J <jgrs.seven@gmail.com>
2024-07-30 09:04:36 -0300

    A

---
+++ poggers/TeladeInicio.xaml.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Maui.Controls;
+
+namespace Pog
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        // Add event handlers for buttons if needed
+        private void OnChecklistClicked(object sender, EventArgs e)
+        {
+            // Handle Checklist button click
+        }
+        
+        private void OnTempoDeProducaoClicked(object sender, EventArgs e)
+        {
+            // Handle Tempo de produção button click
+        }
+        
+        private void OnClienteClicked(object sender, EventArgs e)
+        {
+            // Handle Cliente button click
+        }
+        
+        private void OnProdutoClicked(object sender, EventArgs e)
+        {
+            // Handle Produto button click
+        }
+    }
+}
